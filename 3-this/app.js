8d9e37dd54e5f0e480e9619099727481458ddfc3
Haeun Kim <haeun159951@gmail.com>
2022-01-24 16:50:17 -0500

    4,5,6,8

--- 3-this/app.js
+++ 3-this/app.js
@@ -4,3 +4,22 @@ function hello() {
 }
 
 hello();
+
+class A {
+  constructor(num) {
+    this.num = num;
+  }
+
+  memberFunction() {
+    console.log("----- class -----");
+    console.log(this);
+    console.log(this === global);
+  }
+}
+
+const a = new A(1);
+a.memberFunction();
+
+console.log("===== global scope ===== ");
+console.log(this);
+console.log(this === module.exports);
