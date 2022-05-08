ca1dcaaa1b473e649bb1be246c8340161788c78e
Haeun Kim <haeun159951@gmail.com>
2022-05-08 14:27:36 -0400

    express final

--- 6-express/10-useful-external/app.js
+++ 6-express/10-useful-external/app.js
@@ -1,8 +1,8 @@
-import cookieParser from 'cookie-parser';
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import helmet from 'helmet';
+import cookieParser from "cookie-parser";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import helmet from "helmet";
 
 // http://expressjs.com/en/resources/middleware/morgan.html
 const app = express();
@@ -17,20 +17,20 @@ const app = express();
 // value: yummy_cookie=choco; tasty_cookie=strawberry
 
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ["http://localhost:3000"],
   optionsSuccessStatus: 200, // for options request
   credentials: true, // Access-Control-Allow-Credentials: true
 };
 
 app.use(cookieParser()); // http://expressjs.com/en/resources/middleware/cookie-parser.html
-app.use(morgan('common')); // http://expressjs.com/en/resources/middleware/morgan.html
+app.use(morgan("common")); // http://expressjs.com/en/resources/middleware/morgan.html
 app.use(cors(corsOptions));
 app.use(helmet()); // https://github.com/helmetjs/helmet
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   console.log(req.cookies); // it will be undefined without cookie-parser
   console.log(req.cookies.yummy_cookie);
-  res.send('Welsome!');
+  res.send("Welcome!");
 });
 
 app.listen(8080);
